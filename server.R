b4ac9419d1c498019a9203984ccb4705e74d8777
ChrisBeeley <chris.beeley@gmail.com>
2013-08-26 12:13:31 +0100

    Final commit

---
+++ server.R
@@ -0,0 +1,239 @@
+####################################
+#### Google Analytics - server.R ###
+####################################
+
+library(shiny)
+library(plyr)
+library(ggplot2)
+
+load("analytics.Rdata") # load the dataframe
+
+shinyServer(function(input, output, session){ # pass in a session argument
+  # for extra functionality
+  
+  # keep control of the checkbox control
+  
+  observe({
+    
+    if(class(input$domainShow) != "character"){
+      
+      updateCheckboxGroupInput(session, "domainShow",
+                               choices = list("NHS users" = "NHS",
+                                              "Other" = "Other"),
+                               selected = "NHS users")
+      
+    }
+    
+  })
+  
+  # handle reactive UI 
+  
+  output$reacDomains <- renderUI({
+    
+    domainList = unique(as.character(passData()$networkDomain))
+    
+    selectInput("subDomains", "Choose subdomain", domainList)
+    
+  })
+  
+  # handle custom URL
+  
+  observe({
+    
+    searchString <- parseQueryString(session$clientData$url_search)
+    
+    if(length(searchString)>0){
+      
+      ## deal with first query which indicates the audience
+      
+      if(searchString[[1]] == "nhs"){
+        
+        updateCheckboxGroupInput(session, "domainShow",
+                                 choices = list("NHS users" = "NHS",
+                                                "Other" = "Other"),
+                                 selected = "NHS")
+        
+        updateRadioButtons(session, "outputType",
+                           choices = list("Visitors" = "visitors",
+                                          "Bounce rate" = "bounceRate",
+                                          "Time on site" = "timeOnSite"),
+                           selected= "Bounce rate")
+        
+        updateTabsetPanel(session, inputId = "theTabs", selected = "summary")
+        
+      }
+      
+      if(searchString[[1]] == "other"){
+        
+        updateCheckboxGroupInput(session, "domainShow",
+                                 choices = list("NHS users" = "NHS",
+                                                "Other" = "Other"),
+                                 selected = c("NHS users", "Other"))
+        
+        updateRadioButtons(session, "outputType",
+                           choices = list("Visitors" = "visitors",
+                                          "Bounce rate" = "bounceRate",
+                                          "Time on site" = "timeOnSite"),
+                           selected= "Visitors")
+        
+        updateTabsetPanel(session, inputId = "theTabs", selected = "monthly")
+        
+      }
+      
+      ## do they want a smooth?
+      
+      if(searchString[[2]] == "yes"){
+        
+        updateCheckboxInput(session, inputId = "smoother",
+                            value = TRUE)
+        
+      }
+      
+    }
+    
+    
+  })
+  
+  # prep data once and then pass around the program
+  
+  passData <- reactive({
+    
+    if(input$theTabs != "hourly"){
+      
+      analytics <- analytics[analytics$Date %in% seq.Date(input$dateRange[1], input$dateRange[2], by = "days"),]
+      
+    }
+    
+    if(input$theTabs != "monthly"){
+      
+      analytics <- analytics[analytics$Hour %in% as.numeric(input$minimumTime) : as.numeric(input$maximumTime),]
+      
+    }
+    
+    analytics <- analytics[analytics$Domain %in% unlist(input$domainShow),]
+    
+    analytics
+    
+  })
+  
+  output$monthGraph <- renderPlot({
+    
+    graphData <- ddply(passData(), .(Domain, Date), numcolwise(sum))
+    
+    if(input$outputType == "visitors"){
+      
+      theGraph <- ggplot(graphData, aes(x = Date, y = visitors, group = Domain, colour = Domain)) + geom_line() +
+        ylab("Unique visitors")
+      
+    }
+    
+    if(input$outputType == "bounceRate"){
+      
+      theGraph <- ggplot(graphData, aes(x = Date, y = bounces / visits * 100, group = Domain, colour = Domain)) +
+        geom_line() + ylab("Bounce rate %")
+      
+    }
+    
+    if(input$outputType == "timeOnSite"){
+      
+      theGraph <- ggplot(graphData, aes(x = Date, y = timeOnSite / visits, group = Domain, colour = Domain)) +
+        geom_line() + ylab("Average time on site")
+      
+    }
+    
+    if(input$smoother){
+      
+      theGraph <- theGraph + geom_smooth(method = input$linearModel)
+      
+    }
+    
+    print(theGraph)
+    
+  })
+  
+  output$animateGraph <- renderPlot({
+    
+    graphData <- ddply(passData(), .(Domain, Date), numcolwise(sum))
+    
+    if(input$outputType == "visitors"){
+      
+      theGraph <- ggplot(graphData, aes(x = Date, y = visitors, group = Domain, colour = Domain)) + geom_line() +
+        ylab("Unique visitors")
+      
+    }
+    
+    if(input$outputType == "bounceRate"){
+      
+      theGraph <- ggplot(graphData, aes(x = Date, y = bounces / visits * 100, group = Domain, colour = Domain)) +
+        geom_line() + ylab("Bounce rate %")
+      
+    }
+    
+    if(input$outputType == "timeOnSite"){
+      
+      theGraph <- ggplot(graphData, aes(x = Date, y = timeOnSite / visits, group = Domain, colour = Domain)) +
+        geom_line() + ylab("Average time on site")
+      
+    }
+    
+    smoothData = graphData[graphData$Date %in%
+                             quantile(graphData$Date, input$animation/100, type=1):
+                             quantile(graphData$Date,
+                                      (input$animation+20)/100, type=1),]
+    
+    theGraph <- theGraph + geom_smooth(data = smoothData, method = "lm",
+                                       colour = "black") 
+    
+    print(theGraph)
+    
+  })
+  
+  
+  output$hourGraph <- renderPlot({
+    
+    graphData = ddply(passData(), .(Domain, Hour), numcolwise(sum))
+    
+    if(input$outputType == "visitors"){
+      
+      theGraph <- ggplot(graphData, aes(x = Hour, y = visitors, group = Domain, colour = Domain)) + geom_line() +
+        ylab("Unique visitors")
+      
+    }
+    
+    if(input$outputType == "bounceRate"){
+      
+      theGraph <- ggplot(graphData, aes(x = Hour, y = bounces / visits * 100, group = Domain, colour = Domain)) +
+        geom_line() + ylab("Bounce rate %")
+      
+    }
+    
+    if(input$outputType == "timeOnSite"){
+      
+      theGraph <- ggplot(graphData, aes(x = Hour, y = timeOnSite / visits, group = Domain, colour = Domain)) +
+        geom_line() + ylab("Average time on site")
+      
+    }
+    
+    if(input$smoother){
+      
+      theGraph <- theGraph + geom_smooth(method = "loess")
+      
+    }
+    
+    print(theGraph)
+    
+  })
+  
+  
+  output$textDisplay <- renderText({
+    
+    if(is.null(input$dateRange)) return()
+    
+    paste(
+      length(seq.Date(input$dateRange[1], input$dateRange[2], by = "days")),
+      " days are summarised. There were", sum(passData()$visitors),
+      "visitors in this time period."
+    )
+    
+  })
+})
---
+++ ui.R
@@ -0,0 +1,82 @@
+###############################
+### Google Analytics - ui.R ###
+###############################
+
+library(shiny) 
+
+shinyUI(pageWithSidebar( 
+  
+  headerPanel("Google Analytics"), 
+  
+  sidebarPanel(
+    
+    conditionalPanel(
+      condition = "input.theTabs != 'hourly'",      
+      dateRangeInput(inputId = "dateRange",  
+                     label =  "Date range",
+                     start = "2013-04-01",
+                     max = Sys.Date()
+      )
+    ),
+    
+    checkboxInput(inputId = "smoother",
+                  label = "Add smoother?",
+                  value = FALSE),
+    
+    conditionalPanel(
+      condition = "input.smoother == true",
+      selectInput("linearModel", "Linear or smoothed",
+                  list("lm", "loess"))
+    ),
+    
+    conditionalPanel(
+      condition = "input.theTabs != 'monthly'",      
+      sliderInput(inputId = "minimumTime",
+                  label = "Hours of interest- minimum",
+                  min = 0,
+                  max = 23,
+                  value = 0,
+                  step = 1
+      ),
+
+      sliderInput(inputId = "maximumTime",
+                label = "Hours of interest- maximum",
+                min = 0,
+                max = 23,
+                value = 23,
+                step = 1)
+    ),
+    
+    checkboxGroupInput(inputId = "domainShow",
+                       label = "Show NHS and other domain (defaults to all)?",
+                       choices = list("NHS users" = "NHS",
+                                      "Other" = "Other"),
+                       selected = "NHS users"
+    ),
+    
+    radioButtons(inputId = "outputType",
+                 label = "Output required",
+                 choices = list("Visitors" = "visitors",
+                                "Bounce rate" = "bounceRate",
+                                "Time on site" = "timeOnSite")),
+    
+    sliderInput("animation", "Trend over time",
+                min = 0, max = 80, value = 0, step = 5, 
+                animate=animationOptions(interval=1000, loop=FALSE)),
+    
+   uiOutput("reacDomains")
+    
+  ),
+  mainPanel(
+    tabsetPanel(id ="theTabs",
+                tabPanel("Summary", textOutput("textDisplay"),
+                         textOutput("queryText"), value = "summary"), 
+                tabPanel("Monthly figures", plotOutput("monthGraph"),
+                         value = "monthly"),
+                tabPanel("Hourly figures", plotOutput("hourGraph"),
+                         value = "hourly"),
+                tabPanel("Animated trend", plotOutput("animateGraph"),
+                         value = "animTab")
+    )
+  )
+))
